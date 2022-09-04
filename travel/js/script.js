6d8cfa996af9db08170b783036d8f8c35d76c312
aungminhein17 <aungminhein20202020@gmail.com>
2022-09-04 22:58:31 +0630

    My blog

---
+++ travel/js/script.js
@@ -0,0 +1,26 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .nav');
+
+menu.onclick = () =>{
+   menu.classList.toggle('fa-times');
+   navbar.classList.toggle('active');
+}
+var swiper = new Swiper(".home-slider", {
+  spaceBetween: 0,
+  loop:true,
+  speed: 900,
+  effect: 'fade',
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
